6bc5c25e09fb4a641e78cb5265df583b131431b6
anapauluk-pixel <ana.pauluk@unimedcascavel.coop.br>
2026-09-14 16:17:52 -0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
+const viewSelector =
+document.getElementById('viewSelector');
 
+viewSelector.addEventListener('change', function(){
+
+    document
+    .querySelectorAll('.view-content')
+    .forEach(view => {
+
+        view.style.display = 'none';
+
+    });
+
+    document
+    .getElementById(this.value)
+    .style.display = 'block';
+
+});
